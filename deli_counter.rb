7eaa487447f9084afd9be321062dcdfd85548083
Kirsten Keels <keelsk@gmail.com>
2020-02-23 17:10:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,10 @@ def line(array)
   else
     counter = 0
     while < array.length
+      line_number = counter + 1
+      message = " " + line_number + ". " + array[counter]
+      counter += 1
     end
+    puts "The line is currently:" + message
   end
 end
