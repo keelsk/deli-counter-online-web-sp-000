06e2ba094120ea9db77df7c2298c474557db4c6f
Kirsten Keels <keelsk@gmail.com>
2020-02-23 17:16:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,10 @@ def line(array)
   if (array.length == 0)
     puts "The line is currently empty."
   else
+    message = ""
     array.each_with_index do |person, index|
       line_number = index + 1
-      message = " " + line_number.to_s + ". " + person
+      message = message + " " + line_number.to_s + ". " + person
     end
     puts "The line is currently:" + message
   end
